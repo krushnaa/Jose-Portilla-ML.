1e6ded680d38d58fcd60e9e3637d0300add0e64a
StefanDraghiciTraining <draghicistefan1984@gmail.com>
2017-10-07 22:19:51 +0300

    tf start

---
+++ 9. Neural Nets/tensorflow.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 22:13:17 2017
+
+@author: Stefan Draghici
+"""
+
+import tensorflow as tf
+
+hello_tf=tf.constant('hello TF')
+x=tf.constant(100)
+
+session=tf.Session()
